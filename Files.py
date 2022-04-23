45f854c7d2ff085c44a8f9f32052cb178bd0c89a
Animesh Sahu <animeshsahu19@yahoo.com>
2022-04-23 13:22:17 +0530

    Improve navbar and add dynamic content based on file opened or not

--- Files.py
+++ Files.py
@@ -6,10 +6,6 @@ import javalang
 import shutil
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-if len(sys.argv) != 2:
-    print("Must give a directory from where to serve the files")
-    exit()
-
 def parser(file_name) :
     with open(file_name,'r') as file:
         data = file.read()
@@ -19,30 +15,43 @@ def parser(file_name) :
 def search_files(directory, extension):
     extension = extension.lower()
     for dirpath, dirnames, files in os.walk(directory):
-        files_list = [os.path.join(dirpath, dirname) for dirname in dirnames if not dirname.startswith(".")] + [os.path.join(dirpath, name) for name in files if name.endswith(extension)]
+        dirs_list = [os.path.join(dirpath, dirname).removeprefix("./") + "/" for dirname in dirnames if not dirpath.startswith("./.") and not dirname.startswith(".")]
+        files_list = [os.path.join(dirpath, name).removeprefix("./") for name in files if not dirpath.startswith("./.") and name.endswith(extension)]
         for path in files_list:
-            if extension and path.lower().endswith(extension):
-                print(path)
-                createhtml(path, files_list)
-            elif not extension:
-                print(path)
+            createhtml(path, dirs_list, files_list)
 
-def createhtml(file, files_list):
-    print(file)
+def createhtml(file, dirs_list, files_list):
+    print("Generating for", file)
     cache_path = ".code-cache"
-    cssfilepath = os.path.join(cache_path, "template.css")
-    shutil.copy("./template.css", cssfilepath)
-    htmlfilepath = os.path.join(cache_path, os.path.splitext(file)[0] + ".html")
+    os.makedirs(cache_path, exist_ok=True)
+    shutil.copy("./template.css", os.path.join(cache_path, "template.css"))
+
+    htmlfilepath = os.path.join(cache_path, os.path.join(os.path.dirname(file), "index.html"))
+    html = template.render(nav_dir_list=dirs_list, nav_file_list=[os.path.splitext(f)[0] for f in files_list])
     os.makedirs(os.path.dirname(htmlfilepath), exist_ok=True)
-    html = template.render(files_list=files_list)
+    print(htmlfilepath)
     with open(htmlfilepath, 'w') as htmlfile:
         htmlfile.write(html)
 
-env = Environment(
-    loader=FileSystemLoader("."),
-    autoescape=select_autoescape()
-)
-template = env.get_template("template.html")
+    htmlfilepath = os.path.join(cache_path, os.path.splitext(file)[0] + ".html")
+    data = ['test data']
+    html = template.render(nav_dir_list=dirs_list, nav_file_list=[os.path.splitext(f)[0] for f in files_list], filename=file, data=data)
+    os.makedirs(os.path.dirname(htmlfilepath), exist_ok=True)
+    print(htmlfilepath)
+    with open(htmlfilepath, 'w') as htmlfile:
+        htmlfile.write(html)
 
-path = sys.argv[1]
-search_files(path,"java")
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Must give a directory from where to serve the files")
+        exit()
+
+    env = Environment(
+        loader=FileSystemLoader("."),
+        autoescape=select_autoescape()
+    )
+    template = env.get_template("template.html")
+
+    path = sys.argv[1]
+    search_files(path,"java")
